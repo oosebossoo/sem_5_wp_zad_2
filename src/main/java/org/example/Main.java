c1c61b9912761c7d070fc0286ac2843e5c34aaa6
oosebossoo <sebek.kasprzak@gmail.com>
2025-02-01 12:25:25 +0100

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,75 +1,37 @@
 package org.example;
 
 public class Main {
-    // Liczba filozofów i widelców
-    private static final int NUM_PHILOSOPHERS = 5;
+    // Prywatne statyczne pole przechowujące instancję klasy
+    private static volatile Singleton instance;
 
-    // Tablica widelców (każdy widelec to obiekt do synchronizacji)
-    private static final Object[] forks = new Object[NUM_PHILOSOPHERS];
-
-    // Inicjalizacja widelców
-    static {
-        for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-            forks[i] = new Object();
-        }
+    // Prywatny konstruktor, aby uniemożliwić tworzenie instancji z zewnątrz
+    private Singleton() {
+        // Inicjalizacja obiektu
     }
 
-    // Klasa reprezentująca filozofa
-    static class Philosopher implements Runnable {
-        private int id;
-
-        public Philosopher(int id) {
-            this.id = id;
-        }
-
-        @Override
-        public void run() {
-            try {
-                while (true) {
-                    think();
-                    eat();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        // Symulacja myślenia
-        private void think() throws InterruptedException {
-            System.out.println("Filozof " + id + " myśli.");
-            Thread.sleep((long) (Math.random() * 1000));
-        }
-
-        // Symulacja jedzenia
-        private void eat() throws InterruptedException {
-            // Ustalanie kolejności podnoszenia widelców, aby uniknąć zakleszczenia
-            Object firstFork = forks[id];
-            Object secondFork = forks[(id + 1) % NUM_PHILOSOPHERS];
-
-            // Jeśli filozof ma nieparzysty numer, najpierw bierze lewy widelec, potem prawy
-            // Jeśli filozof ma parzysty numer, najpierw bierze prawy widelec, potem lewy
-            if (id % 2 == 0) {
-                synchronized (firstFork) {
-                    synchronized (secondFork) {
-                        System.out.println("Filozof " + id + " je.");
-                        Thread.sleep((long) (Math.random() * 1000));
-                    }
-                }
-            } else {
-                synchronized (secondFork) {
-                    synchronized (firstFork) {
-                        System.out.println("Filozof " + id + " je.");
-                        Thread.sleep((long) (Math.random() * 1000));
-                    }
+    // Publiczna metoda statyczna zwracająca instancję klasy
+    public static Singleton getInstance() {
+        // Pierwsze sprawdzenie (bez synchronizacji)
+        if (instance == null) {
+            // Synchronizacja tylko przy pierwszym tworzeniu instancji
+            synchronized (Singleton.class) {
+                // Drugie sprawdzenie (wewnątrz synchronizacji)
+                if (instance == null) {
+                    instance = new Singleton();
                 }
             }
         }
+        return instance;
+    }
+
+    // Przykładowa metoda instancyjna
+    public void doSomething() {
+        System.out.println("Singleton działa!");
     }
 
     public static void main(String[] args) {
-        // Tworzenie i uruchamianie wątków filozofów
-        for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-            new Thread(new Philosopher(i)).start();
-        }
+        // Przykład użycia
+        Singleton singleton = Singleton.getInstance();
+        singleton.doSomething();
     }
 }
